a61e16abd5cb520cc45ed4bee6124c62fcb46685
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-22 18:19:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Meu time</h1>
+      <label htmlFor="key">key de autenticação:</label>
+      <input className='key' type="text" />
+      <button>Entrar</button>
+    </div>
+  )
+}
+
+export default App
