02088100c8492ce910309079cf3f22b8b4321bd1
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-22 18:22:44 -0300

    will create the parent component

---
+++ src/components/layoult/Container.jsx
@@ -0,0 +1,9 @@
+import styles from "../layoult/Container.module.css";
+
+const Container=(props)=> {
+    return <div className={`${styles.container} ${styles[props.customClass]}`}>{props.children}</div>
+    //quer dizer que os elementos filhos ficarão encapsulados dentro de Container.
+    //classes dinâmicas. Coloco a classe que vêm da props.(customClass). É opcional, quando for necessário.
+}
+
+export default Container
