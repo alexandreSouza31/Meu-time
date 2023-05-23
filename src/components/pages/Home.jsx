f9630de73845b7b776f254f74eee0974bbe88a1a
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-22 21:05:25 -0300

    will create the main page component

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,15 @@
+import LinkButton from "../layoult/LinkButton";
+
+const Home = () => {
+    return (
+        <section >
+            <h1>
+                Bem-vindo
+            </h1>
+            <LinkButton to="/" text="Sair"/>
+
+        </section>
+    )
+}
+
+export default Home;
