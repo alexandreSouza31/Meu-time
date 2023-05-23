2d4a61bd665d5f1c1ef50d85639c7f0e4a09da5c
alexandreSouza31 <alexandre31_m@hotmail.com>
2023-05-22 23:18:26 -0300

    will create the basic structure for the main page

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,14 +1,38 @@
+import styles from "./Home.module.css";
+
 import LinkButton from "../layoult/LinkButton";
 
 const Home = () => {
     return (
-        <section >
-            <h1>
-                Bem-vindo
-            </h1>
-            <LinkButton to="/" text="Sair"/>
+        <div className={styles.container}>
+            <nav className={styles.nav}>
+                <h1>Bem-vindo</h1>
+                <LinkButton to="/" text="Sair" />
+            </nav>
 
-        </section>
+            <main className={styles.main}>
+                <h3>Selecione os campos abaixo para ver as estatísticas de um time:</h3>
+                <div className={styles.container_main}>
+
+                    <label htmlFor="country">Seleceione um país:</label>
+                    <select name="" className={styles.country}>
+                        <option value="">Selecione</option>
+                    </select>
+
+                    <label htmlFor="league">Seleceione a liga:</label>
+                    <select name="" className={styles.league}>
+                        <option value="">Selecione</option>
+                    </select>
+
+                    <label htmlFor="team">Seleceione o time:</label>
+                    <select name="" className={styles.team}>
+                        <option value="">Selecione</option>
+                    </select>
+
+                    <button>Ver time</button>
+                </div>
+            </main>
+        </div>
     )
 }
 
